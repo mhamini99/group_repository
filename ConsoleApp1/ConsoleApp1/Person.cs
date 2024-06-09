841905fcebf2413bec85d11c03bd59e40e49e741
MaryamSalehi <tiamsalehi22148@gmail.com>
2024-06-09 07:39:37 -0700

    Create Queue class and constructor for person class

--- ConsoleApp1/ConsoleApp1/Person.cs
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -11,5 +11,12 @@ namespace ConsoleApp1
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public Person(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
